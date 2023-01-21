5474df990ee088bdb1efff3186b26b213aba064e
Codina <codina@cogiti.com>
2023-01-21 21:05:23 +0100

    added string replace trigger button

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function machwas () {
     let element = document.getElementsByTagName("p")[0];
     element.innerHTML += " dazu";
 }
+
+function tunochwas () {
+    let element = document.getElementsByTagName("p")[0];
+    element.innerHTML = element.innerHTML.replaceAll("zu", "hin");
+}
